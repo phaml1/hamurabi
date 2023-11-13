401c6932794350f63be2522e7ca5e41c1044b54f
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-11-12 19:11:13 -0500

    added some methods to work on later

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -11,9 +11,27 @@ public class Hammurabi {         // must save in a file named Hammurabi.java
     }
 
     void playGame() {
+    int deaths, immigrants = 5, population = 100, acres = 1000, stores = 2800;
         // declare local variables here: grain, population, etc.
         // statements go after the declations
     }
+
+
+
+
+    public int plagueDeaths(int num){return 0;}
+
+    public int starvationDeaths(int num, int num2){return 0;}
+
+    public boolean uprising(int num, int num2){return false;}
+
+    public int immigrants(int num, int num2, int num3){return 0;}
+
+    public int harvest(int num){return 0;}
+
+    public int grainEatenByRats(int num){return 0;}
+
+    public int newCostOfLand(){return 0;}
 
     //other methods go here
 
